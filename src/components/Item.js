0e26c17d35cf10bcd43c6735d864db158f85f581
Naseem77 <34807727+Naseem77@users.noreply.github.com>
2022-06-20 17:34:57 +0300

    init

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react'
 
 class Item extends Component {
-  checkItem = () => {
-    //your code here
+  checkItem = (e) => {
+    this.props.store.checkItem(e.target.value)
   }
   editItem = () => {
-    //your code here
+    const itemName = this.props.item.name
+    const newLocation = prompt("Whats the new location you want?")
+    this.props.store.editItem(itemName, newLocation)
   }
+
   deleteItem = () => {
-    //your code here
+    const itemName = this.props.item.name
+    this.props.store.deleteItem(itemName)
   }
+
   render() {
+    let item = this.props.item
     return (
-      <div className="">
-        {/*   your code here
-            each item should be in an input checkbox
-            it should display the item name and location
-            it should have both an edit button and a delete button
-      */}
+      <div className={item.completed ? "crossed" : null}>
+        <input type="checkbox"
+          onClick={this.checkItem}
+          value={item.name} />
+        {item.name} {" "} {item.location}
+        <button onClick={this.editItem}>Edit</button>
+        <button onClick={this.deleteItem}>Delete</button>
       </div>)
   }
 }
--- src/stores/ShoppingList.js
+++ src/stores/ShoppingList.js
@@ -1,26 +1,34 @@
-/* eslint-disable */
 import { observable, action, makeObservable } from 'mobx'
 import { Item } from './Item'
 
-
 export class ShoppingList {
     constructor() {
-        this.list = []
-        this.length = 0
-        // your code here
+        this.list = [];
+        this.length = 0;
+
+        makeObservable(this, {
+            list: observable,
+            length: observable,
+            checkItem: action,
+            addItem: action,
+            editItem: action,
+            deleteItem: action
+        })
+    }
+    checkItem = (name) => {
+        let item = this.list.find(i => i.name === name)
+        item.completed = !item.completed
+    }
+    addItem = (name) => {
+        this.list.push(new Item(name))
+    }
+    editItem = (itemName, newLocation) => {
+        let item = this.list.find(i => i.name === itemName)
+        item.location = newLocation
 
     }
-    checkItem = () => {
-        // your code here
-    }
-    addItem = () => {
-        // your code here
-    }
-    editItem = () => {
-        // your code here
-    }
-    deleteItem = () => {
-        // your code here
+    deleteItem = (name) => {
+        let itemIndex = this.list.findIndex(i => i.name === name)
+        this.list.splice(itemIndex, 1)
     }
 }
-
